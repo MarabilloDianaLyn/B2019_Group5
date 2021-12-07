b90347189d08d3a10f3c39bfed88de3b6b4483ae
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2021-12-07 23:50:18 +0800

    act2

--- Notifications/app/src/main/java/com/example/notify/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notify/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.notify;
-
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.INotificationSideChannel;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_Notify = (Button)findViewById(R.id.button_show);
+        btn_Notify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this);
+                mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground);
+                mBuilder.setContentTitle("Tutlane Send New Message");
+                mBuilder.setContentText("Hi, Welcome to Tutlane tutorial site");
 
+                Intent resultIntent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, resultIntent, 0);
+                int mNotificationId = 001;
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                notificationManager.notify(mNotificationId, mBuilder.build());
+            }
+        });
     }
 }
