060084b5890f88bccace69f451f587a38d21c062
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2021-12-08 01:03:51 +0800

    act2

--- Notifications/app/src/main/java/com/example/notify/MainActivity.java
+++ Notifications/app/src/main/java/com/example/notify/MainActivity.java
@@ -15,16 +15,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnnotif;
-    Button btnbig;
+    Button notif_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnnotif = findViewById(R.id.button_show);
-        btnbig = findViewById(R.id.btn_Big);
+        notif_btn = findViewById(R.id.button_show);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel = new NotificationChannel("My Notification","My Notification", NotificationManager.IMPORTANCE_DEFAULT);
@@ -32,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             manager.createNotificationChannel(channel);
 
         }
-        btnnotif.setOnClickListener(new View.OnClickListener() {
+        notif_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
@@ -44,27 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 managerCompat.notify( 1,builder.build());
             }
         });
-        btnbig.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-// Assign big picture notification
-                NotificationCompat.BigPictureStyle bpStyle = new NotificationCompat.BigPictureStyle();
-                bpStyle.bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground)).build();
-// Set the intent to fire when the user taps on
-                Intent rIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://tutlane.com/"));
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, rIntent, 0);
-                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Big Picture Notification Example")
-                        .addAction(R.drawable.ic_launcher_foreground, "Share", pendingIntent)
-                        .setStyle(bpStyle);
-                mBuilder.setContentIntent(pendingIntent);
-// Sets an ID for the notification
-                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-// It will display the notification in notification bar
-                notificationManager.notify(02, mBuilder.build());
-            }
-        });
+
 
     }
 }
