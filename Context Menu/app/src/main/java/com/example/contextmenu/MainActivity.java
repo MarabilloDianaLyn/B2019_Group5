60227120f36cebe1a84dd4ba842e316c910116a9
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2021-12-08 17:56:29 +0800

    group5

--- Context Menu/app/src/main/java/com/example/contextmenu/MainActivity.java
+++ Context Menu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button m_btn = (Button) findViewById(R.id.buttonn);
+        Button m_btn = (Button) findViewById(R.id.buttonshow);
         registerForContextMenu(m_btn);
     }
 
