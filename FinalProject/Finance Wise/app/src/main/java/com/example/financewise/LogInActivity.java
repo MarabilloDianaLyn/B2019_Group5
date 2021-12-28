344d4a28c2c4fad3bba0d2a73186b1836c4d52d3
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2021-12-29 00:59:08 +0800

    firebase added

--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/LogInActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/LogInActivity.java
@@ -1,19 +1,31 @@
 package com.example.financewise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
     private EditText email, password;
     private Button login_btn;
     private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
 
     @Override
@@ -26,11 +38,50 @@ public class LogInActivity extends AppCompatActivity {
         login_btn = findViewById(R.id.login_btn);
         loginQn = findViewById(R.id.loginQn);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
         loginQn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LogInActivity.this, RegistrationActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if (TextUtils.isEmpty(emailString)){
+                    email.setError("Email is required");
+                }
+                if (TextUtils.isEmpty(passwordString)){
+                    password.setError("Password is required");
+                }
+                else{
+                    progressDialog.setMessage("Logging in");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                               Intent intent = new Intent(LogInActivity.this, DashboardActivity.class);
+                               startActivity(intent);
+                               finish();
+                               progressDialog.dismiss();
+                            }
+                            else{
+                                Toast.makeText(LogInActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
@@ -1,19 +1,31 @@
 package com.example.financewise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText rgstr_name, rgstr_contactno, rgstr_email, rgstr_password;
     private Button rgstr_btn;
     private TextView rgstrloginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,50 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View view){
                 Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        rgstr_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = rgstr_email.getText().toString();
+                String passwordString = rgstr_password.getText().toString();
+                String nameString = rgstr_name.getText().toString();
+                String contactString = rgstr_contactno.getText().toString();
+
+                if (TextUtils.isEmpty(nameString)){
+                    rgstr_email.setError("Your full name is required");
+                }
+                if (TextUtils.isEmpty(contactString)){
+                    rgstr_email.setError("Contact number is required");
+                }
+                if (TextUtils.isEmpty(emailString)){
+                    rgstr_email.setError("Email is required");
+                }
+                if (TextUtils.isEmpty(passwordString)){
+                    rgstr_password.setError("Password is required");
+                }
+                else {
+                    progressDialog.setMessage("Signing up");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(RegistrationActivity.this, DashboardActivity.class);
+                                startActivity(intent);
+                                finish();
+                                progressDialog.dismiss();
+                            }
+                            else{
+                                Toast.makeText(RegistrationActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
