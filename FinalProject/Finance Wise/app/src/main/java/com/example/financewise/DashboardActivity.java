235c90d401bd55bf84f3b9ed43e38fbc07991615
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2022-01-10 22:32:09 +0800

    twonewclass

--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/DashboardActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/DashboardActivity.java
@@ -2,13 +2,39 @@ package com.example.financewise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button expense_Btn, income_Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //code for analytics
+
+        //-----------
+
+        expense_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ExpenseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        income_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (DashboardActivity.this, IncomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
