413c84e247dece875d97725d4f3400ad13996ac7
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2022-01-27 21:38:45 +0800

    try

--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
@@ -19,14 +19,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText rgstr_name, rgstr_contactno, rgstr_email, rgstr_password;
     private Button rgstr_btn;
     private TextView rgstrloginQn;
-
-    private FirebaseAuth mAuth;
-    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +43,6 @@ public class RegistrationActivity extends AppCompatActivity {
         rgstr_btn = findViewById(R.id.rgstr_btn);
         rgstrloginQn = findViewById(R.id.rgstrloginQn);
 
-        mAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(this);
-
         rgstrloginQn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -51,48 +51,64 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
 
-       rgstr_btn.setOnClickListener(new View.OnClickListener() {
+        rgstr_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailString = rgstr_email.getText().toString();
-                String passwordString = rgstr_password.getText().toString();
-                String nameString = rgstr_name.getText().toString();
-                String contactString = rgstr_contactno.getText().toString();
+                if (rgstr_name.getText().toString().equals("")){
+                    Toast.makeText(RegistrationActivity.this, "Enter full name", Toast.LENGTH_SHORT).show();
+                }else if(rgstr_contactno.getText().toString().equals("")){
+                    Toast.makeText(RegistrationActivity.this, "Enter contact number", Toast.LENGTH_SHORT).show();
+                }else if(rgstr_email.getText().toString().equals("")){
+                    Toast.makeText(RegistrationActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                }else if(rgstr_password.getText().toString().equals("")){
+                    Toast.makeText(RegistrationActivity.this, "Enter passwordd", Toast.LENGTH_SHORT).show();
+                }
 
-                if (TextUtils.isEmpty(nameString)){
-                    rgstr_email.setError("Your full name is required");
-                }
-                if (TextUtils.isEmpty(contactString)){
-                    rgstr_email.setError("Contact number is required");
-                }
-                if (TextUtils.isEmpty(emailString)){
-                    rgstr_email.setError("Email is required");
-                }
-                if (TextUtils.isEmpty(passwordString)){
-                    rgstr_password.setError("Password is required");
-                }
-                else {
-                    progressDialog.setMessage("Signing up");
-                    progressDialog.setCanceledOnTouchOutside(false);
-                    progressDialog.show();
+                HashMap<String, String> params = new HashMap<>();
+                params.put("fullname", rgstr_name.getText().toString());
+                params.put("contactno", rgstr_contactno.getText().toString());
+                params.put("email", rgstr_contactno.getText().toString());
+                params.put("password", rgstr_password.getText().toString());
+                fw_register(params);
 
-                    mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
-                                startActivity(intent);
-                                finish();
-                                progressDialog.dismiss();
-                            }
-                            else{
-                                Toast.makeText(RegistrationActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
-                                progressDialog.dismiss();
-                            }
-                        }
-                    });
-                }
+
             }
         });
+    }
+
+    private void fw_register(HashMap <String, String> params){
+
+        final ProgressDialog progressDialog = new ProgressDialog(RegistrationActivity.this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Registering...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        NetworkService networkService = NetworkClient.getClient().create(NetworkService.class);
+        Call<RegisterResponseModel> registerCall = networkService.fw_register(params);
+        registerCall.enqueue(new Callback<RegisterResponseModel>() {
+            @Override
+            public void onResponse(Call<RegisterResponseModel> call, Response<RegisterResponseModel> response) {
+                RegisterResponseModel responseBody = response.body();
+                if (responseBody != null){
+                    if (responseBody.getSuccess().equals("1")){
+                        Toast.makeText(RegistrationActivity.this, responseBody.getMessage(), Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Toast.makeText(RegistrationActivity.this, responseBody.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<RegisterResponseModel> call, Throwable t) {
+                progressDialog.dismiss();
+
+            }
+        });
+
     }
 }
