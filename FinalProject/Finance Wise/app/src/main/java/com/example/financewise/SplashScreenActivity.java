501cd6cbca93c3b6bcb23cd87c5453ebf2034976
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2022-01-30 11:35:02 +0800

    Update SplashScreenActivity.java

--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/SplashScreenActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/SplashScreenActivity.java
@@ -24,6 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
