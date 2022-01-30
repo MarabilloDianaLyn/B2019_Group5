aa5f7b01925a66793aff56d79f68247c38bba5b0
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2022-01-30 10:46:24 +0800

    db

--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/LogInActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/LogInActivity.java
@@ -1,86 +1,85 @@
 package com.example.financewise;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class LogInActivity extends AppCompatActivity {
 
-    private EditText email, password;
-    private Button login_btn;
-    private TextView loginQn;
-
-    private FirebaseAuth mAuth;
-    private ProgressDialog progressDialog;
+    TextInputEditText textInputEditTextUsername, textInputEditTextPassword;
+    Button login_btn;
+    TextView loginQn;
+    ProgressBar progressBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+        textInputEditTextUsername = findViewById(R.id.loginusername);
+        textInputEditTextPassword = findViewById(R.id.loginpassword);
         login_btn = findViewById(R.id.login_btn);
-        loginQn = findViewById(R.id.loginQn);
-
-        mAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(this);
+        loginQn = findViewById(R.id.rgstrloginQn);
+        progressBar = findViewById(R.id.progress);
 
         loginQn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LogInActivity.this, RegistrationActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
-        login_btn.setOnClickListener(new View.OnClickListener(){
+        login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailString = email.getText().toString();
-                String passwordString = password.getText().toString();
+                String username, password;
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
 
-                if (TextUtils.isEmpty(emailString)){
-                    email.setError("Email is required");
-                }
-                if (TextUtils.isEmpty(passwordString)){
-                    password.setError("Password is required");
-                }
-                else{
-                    progressDialog.setMessage("Logging in");
-                    progressDialog.setCanceledOnTouchOutside(false);
-                    progressDialog.show();
-
-                    mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if(!username.equals("") && !password.equals("")){
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                               Intent intent = new Intent(LogInActivity.this, DashboardActivity.class);
-                               startActivity(intent);
-                               finish();
-                               progressDialog.dismiss();
-                            }
-                            else{
-                                Toast.makeText(LogInActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
-                                progressDialog.dismiss();
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("https://b2019cc107group5.000webhostapp.com/login.php", "POST", field, data);
+                            if (putData.startPut()){
+                                if (putData.onComplete()){
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Sign up success")){
+                                        Intent intent = new Intent(LogInActivity.this, DashboardActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
                             }
                         }
                     });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
+++ FinalProject/Finance Wise/app/src/main/java/com/example/financewise/RegistrationActivity.java
@@ -5,81 +5,90 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText rgstr_name, rgstr_contactno, rgstr_email, rgstr_password;
-    private Button rgstr_btn;
-    private TextView rgstrloginQn;
+    TextInputEditText textInputEditTextFullname, textInputEditTextUsername,
+            textInputEditTextPassword,textInputEditTextEmail;
+    Button btn_register;
+    TextView qn_register;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        rgstr_name = findViewById(R.id.rgstr_name);
-        rgstr_contactno = findViewById(R.id.rgstr_contactno);
-        rgstr_email = findViewById(R.id.rgstr_email);
-        rgstr_password = findViewById(R.id.rgstr_password);
-        rgstr_btn = findViewById(R.id.rgstr_btn);
-        rgstrloginQn = findViewById(R.id.rgstrloginQn);
+        textInputEditTextFullname = findViewById(R.id.fullname);
+        textInputEditTextUsername = findViewById(R.id.username);
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        btn_register = findViewById(R.id.rgstr_btn);
+        qn_register = findViewById(R.id.rgstrloginQn);
+        progressBar = findViewById(R.id.progress);
 
-        rgstrloginQn.setOnClickListener(new View.OnClickListener(){
+        qn_register.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
-        rgstr_btn.setOnClickListener(new View.OnClickListener() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullname, email, password;
-                fullname = rgstr_name.getText().toString();
-                email = rgstr_email.getText().toString();
-                password = rgstr_password.getText().toString();
+                String fullname, email, username, password;
+                fullname = String.valueOf(textInputEditTextFullname.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
 
-                if (!fullname.equals("") && !email.equals("") && !password.equals("")){
-
+                if(!fullname.equals("") && !email.equals("") && !username.equals("") && !password.equals("")){
+                    progressBar.setVisibility(View.VISIBLE);
                     Handler handler = new Handler();
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                           String[] field = new String[3];
+                            String[] field = new String[4];
                             field[0] = "fullname";
-                            field[1] = "email";
+                            field[1] = "username";
                             field[2] = "password";
-                            String[] data = new String[3];
+                            field[3] = "email";
+                            String[] data = new String[4];
                             data[0] = fullname;
-                            data[1] = email;
+                            data[1] = username;
                             data[2] = password;
+                            data[3] = email;
                             PutData putData = new PutData("https://b2019cc107group5.000webhostapp.com/signup.php", "POST", field, data);
-
-                            if (putData.onComplete()) {
-                                String result = putData.getResult();
-                                if (result.equals("Sign Up Success")) {
-                                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                            if (putData.startPut()){
+                                if (putData.onComplete()){
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Sign up success")){
+                                        Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             }
                         }
                     });
                 }
-                else {
-                    Toast.makeText(getApplicationContext(), "All fields are required!", Toast.LENGTH_SHORT).show();
-
+                else{
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
                 }
             }
         });
