abce94dcdcf59539b95abaaf0a221f26db649ca0
MarabilloDianaLyn <94978615+MarabilloDianaLyn@users.noreply.github.com>
2022-02-01 14:53:14 +0800

    Update SplashScreenActivity.java

--- FinalProject/FinanceWIse/app/src/main/java/com/example/financewise/SplashScreenActivity.java
+++ FinalProject/FinanceWIse/app/src/main/java/com/example/financewise/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.example.financewise;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,39 +11,37 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.financewise.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static int SPLASH = 3000;
     Animation animation;
     private ImageView imageView;
-    private TextView appname;
+    private TextView appName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_splashscreen);
 
         animation = AnimationUtils.loadAnimation(this, R.anim.animation);
 
         imageView = findViewById(R.id.imageView);
-        appname = findViewById(R.id.appname);
+        appName = findViewById(R.id.appname);
 
         imageView.setAnimation(animation);
-        appname.setAnimation(animation);
+        appName.setAnimation(animation);
 
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 Intent intent = new Intent(SplashScreenActivity.this, LogInActivity.class);
                 startActivity(intent);
                 finish();
             }
-
         }, SPLASH);
     }
 }
